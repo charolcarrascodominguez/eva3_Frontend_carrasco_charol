377a603cc28ac90b1c55d85f6a10fe3e425ca954
Charol Carrasco <charolcarrascodominguez@gmail.com>
2026-06-06 10:56:04 -0400

    Evaluacion Final Charol Carrasco inicial

--- js/api.js
+++ js/api.js
@@ -1,18 +1,30 @@
-/*
-Autor: Charol Carraco
-Archivo: api.js
-Responsabilidad: Comunicación con API (CMS / Backend).
-*/
+import { CONFIG } from "./config.js";
 
-const API_URL = "http://localhost:3000";
+export async function fetchAPI(endpoint) {
+    try {
+        const response = await fetch(endpoint);
 
-async function fetchData(endpoint) {
-  try {
-    const response = await fetch(`${API_URL}/${endpoint}`);
-    if (!response.ok) throw new Error("Error en API");
-    return await response.json();
-  } catch (error) {
-    console.error("Error al obtener datos:", error);
-    return [];
-  }
+        if (!response.ok) {
+            throw new Error("Error en servidor");
+        }
+
+        const data = await response.json();
+        return data;
+
+    } catch (error) {
+        console.error("Error API:", error);
+        throw error;
+    }
+}
+
+export function construirURLListado(fecha, estado) {
+    return ${CONFIG.BASE_URL}/publico/licitaciones.json?fecha=${fecha}&estado=${estado}&ticket=${CONFIG.TICKET};
+}
+
+export function construirURLDetalle(codigo) {
+    return ${CONFIG.BASE_URL}/publico/licitaciones.json?codigo=${codigo}&ticket=${CONFIG.TICKET};
+}
+
+export function construirURLProveedor(rut) {
+    return ${CONFIG.BASE_URL}/Publico/Empresas/BuscarProveedor?rutempresaproveedor=${rut}&ticket=${CONFIG.TICKET};
 }
